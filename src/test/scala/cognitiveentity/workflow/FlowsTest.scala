37d7a0fbd07185b6fc3a68ebca7cde0845814765
Richard Searle <harold.richard.searle@gmail.com>
2011-10-30 17:22:27 -0500

    more tests

--- src/test/scala/cognitiveentity/workflow/FlowsTest.scala
+++ src/test/scala/cognitiveentity/workflow/FlowsTest.scala
@@ -12,54 +12,59 @@ object FlowsTest extends org.specs2.mutable.SpecificationWithJUnit {
   implicit val stdBalLook: Lookup[Acct, Bal] = StdBalService
   implicit val special = SpecialService
   implicit val numLook = Service(ValueMaps.numMap)
+  
+  "phones" in {
+    Phones(Id(123)).get must beEqualTo(List(Num("124-555-1234"), Num("333-555-1234")))
+    Phones(Id(-1)).await.exception.get.getMessage() must beEqualTo("key not found: Id(-1)")
+  }
 
   "special" in {
-    SpecialLineBalance.apply(Num("124-555-1234")).get must beEqualTo(Bal(1000F))
+    SpecialLineBalance(Num("124-555-1234")).get must beEqualTo(Bal(1000F))
   }
 
   "other" in {
-    OtherLineBalance.apply(Num("124-555-1234")).get must beEqualTo(Bal(13F))
+    OtherLineBalance(Num("124-555-1234")).get must beEqualTo(Bal(13F))
 
   }
   
    "split" in {
-    SplitLineBalance.apply(Num("124-555-1234")).get must beEqualTo(Bal(1124.5F))
-    SplitLineBalance.apply(Num("124-555-1234")).await.result must beEqualTo(Some(Bal(1124.5F)))
-    val noResult = SplitLineBalance.apply(Num("xxx")).await
+    SplitLineBalance(Num("124-555-1234")).get must beEqualTo(Bal(1124.5F))
+    SplitLineBalance(Num("124-555-1234")).await.result must beEqualTo(Some(Bal(1124.5F)))
+    val noResult = SplitLineBalance(Num("xxx")).await
     noResult.result must beEqualTo(None)
     noResult.exception.get.getMessage() must beEqualTo("key not found: Num(xxx)")
   }
 
   "slb" in {
-    SingleLineBalance.apply(Num("124-555-1234")).get must beEqualTo(Bal(124.5F))
-    SingleLineBalance.apply(Num("124-555-1234")).await.result must beEqualTo(Some(Bal(124.5F)))
-    val noResult = SingleLineBalance.apply(Num("xxx")).await
+    SingleLineBalance(Num("124-555-1234")).get must beEqualTo(Bal(124.5F))
+    SingleLineBalance(Num("124-555-1234")).await.result must beEqualTo(Some(Bal(124.5F)))
+    val noResult = SingleLineBalance(Num("xxx")).await
     noResult.result must beEqualTo(None)
     noResult.exception.get.getMessage() must beEqualTo("key not found: Num(xxx)")
   }
 
   "rnb" in {
-    RecoverNumBalance.apply(Num("124-555-1234")).get must beEqualTo(Bal(124.5F))
-    RecoverNumBalance.apply(null).get must beEqualTo(Bal(0F))
-    RecoverNumBalance.apply(Num("xxxx")).get must beEqualTo(Bal(0.0F))
+    RecoverNumBalance(Num("124-555-1234")).get must beEqualTo(Bal(124.5F))
+    RecoverNumBalance(null).get must beEqualTo(Bal(0F))
+    RecoverNumBalance(Num("xxxx")).get must beEqualTo(Bal(0.0F))
   }
 
   "fnbIf" in {
-    IfNumBalance.apply(Num("124-555-1234")).get must beEqualTo(Bal(124.5F))
-    IfNumBalance.apply(Num("999-555-1234")).get must beEqualTo(Bal(55F))
-    IfNumBalance.apply(null).get must beEqualTo(Bal(0F))
+    IfNumBalance(Num("124-555-1234")).get must beEqualTo(Bal(124.5F))
+    IfNumBalance(Num("999-555-1234")).get must beEqualTo(Bal(55F))
+    IfNumBalance(null).get must beEqualTo(Bal(0F))
   }
 
   "bbm" in {
-    BalanceByMap.apply(Id(123)).get must beEqualTo(Bal(125.5F))
+    BalanceByMap(Id(123)).get must beEqualTo(Bal(125.5F))
   }
 
   "bsbf" in {
-    BalancesByFor.apply(Id(123)).get must beEqualTo(List(Bal(124.5F), Bal(1.0F)))
+    BalancesByFor(Id(123)).get must beEqualTo(List(Bal(124.5F), Bal(1.0F)))
   }
 
   "bsbm" in {
-    BalancesByMap.apply(Id(123)).get must beEqualTo(List(Bal(124.5F), Bal(1.0F)))
+    BalancesByMap(Id(123)).get must beEqualTo(List(Bal(124.5F), Bal(1.0F)))
   }
 
   case class Service[K, V](values: Map[K, V]) extends MapService(values) with Lookup[K, V]
