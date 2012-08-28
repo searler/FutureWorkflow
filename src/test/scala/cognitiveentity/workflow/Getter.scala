f53f68a7f244f100150e153e45ccec1d95728885
Richard Searle <harold.richard.searle@gmail.com>
2012-08-28 17:22:03 -0500

    use mapTo to eliminate compiler warning

--- src/test/scala/cognitiveentity/workflow/Getter.scala
+++ src/test/scala/cognitiveentity/workflow/Getter.scala
@@ -12,11 +12,12 @@ case object Getter {
 }
 
 case class Getter[T](future: Future[T]) {
+  import scala.reflect.ClassTag
   private val duration = Duration(100, TimeUnit.MILLISECONDS)
   def get: T = Await.result(future, duration)
   def exception: String = Await.result(future.failed, duration).getMessage
-  def option(implicit ec: ExecutionContext): Option[T] = Await.result(future.map { case v: T => Some(v) }.recover { case _ => None },
+  def option(implicit ec: ExecutionContext,tag: ClassTag[T]): Option[T] = Await.result(future.mapTo[T].map { case v: T => Some(v) }.recover { case _ => None },
     duration)
-  def asEither(implicit ec: ExecutionContext): Either[Throwable, T] = Await.result(future.map { case v: T => Right(v) }.recover { case _@ e => Left(e) },
+  def asEither(implicit ec: ExecutionContext,tag: ClassTag[T]): Either[Throwable, T] = Await.result(future.mapTo[T].map { case v: T => Right(v) }.recover { case _@ e => Left(e) },
     duration)
 }
